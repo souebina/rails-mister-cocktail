40c42d82459167fc0491b43ed3f8d8383ef22a3d
Sosuke Ebina <ultra6lue@gmail.com>
2017-07-06 17:14:24 +0900

    update

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,24 +1,22 @@
 class DosesController < ApplicationController
+  before_action :set_cocktail, only: [:create]
   def create
-    before_action :set_cocktail, only: [:create]
-    def create
-      @dose = Dose.new(dose_params)
-      @dose.cocktail = @cocktail
-      if @dose.save
-        redirect_to cocktail_path(@cocktail)
-      else
-        render 'cocktails/show'
-      end
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'cocktails/show'
     end
+  end
 
-    private
+  private
 
-    def dose_params
-      params.require(:dose).permit(:content, :rating)
-    end
+  def dose_params
+    params.require(:dose).permit(:content, :rating)
+  end
 
-    def set_cocktail
-      @cocktail = Cocktail.find(params[:cocktail_id])
-    end
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 end
